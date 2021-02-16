e99746f90e29d1dfabe24ad91b6f445c61b3a332
Tiago Lopes <tiago.lopes@criticalsoftware.com>
2021-02-16 18:26:32 +0000

    Initial commit

---
+++ src/main/java/pt/tlopes/nextbookarchiver/NextBookArchiverApplication.java
@@ -0,0 +1,13 @@
+package pt.tlopes.nextbookarchiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextBookArchiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextBookArchiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/tlopes/nextbookarchiver/NextBookArchiverApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.tlopes.nextbookarchiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextBookArchiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
